faaaccc70a4de96ad3be1c432ae7d257d92976da
Alexander ITG <soporteitg@iMac-de-SoporteITG.local>
2022-05-11 16:22:01 -0600

    eso

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ProyectoMundial
 Resultados y Quinielas
+<?php
+$db = new PDO('pgsql:host=localhost');
+$statement = $db->prepare("SELECT datname FROM pg_database");
+$statement->execute();
+while ($row = $statement->fetch()) {
+    echo "<p>" . htmlspecialchars($row["datname"]) . "</p>\n";
+}
+?>
---
+++ conn.php
@@ -0,0 +1,14 @@
+To connect from PHP, make sure that it supports PostgreSQL. The version included with macOS doesn't support PostgreSQL. We recommend MAMP for an easy way to install a current version of PHP that works.
+
+You can use PDO (object oriented):
+
+
+Or the pg_connect() functions (procedural):
+
+<?php
+    $conn = pg_connect("host=localhost, port=5432 dbname='Mundial' ");
+    $result = pg_query($conn, "SELECT * FROM ");
+    while ($row = pg_fetch_row($result)) {
+        echo "<p>" . htmlspecialchars($row[0]) . "</p>\n";
+    }
+?>
