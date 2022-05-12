01294af039269f9b43d781d4c96a365ecd46fbb1
Alexander ITG <soporteitg@iMac-de-SoporteITG.local>
2022-05-12 09:41:27 -0600

    eso

--- conn.php
+++ conn.php
@@ -1,14 +1,18 @@
-To connect from PHP, make sure that it supports PostgreSQL. The version included with macOS doesn't support PostgreSQL. We recommend MAMP for an easy way to install a current version of PHP that works.
-
-You can use PDO (object oriented):
-
-
-Or the pg_connect() functions (procedural):
-
 <?php
-    $conn = pg_connect("host=localhost, port=5432 dbname='Mundial' ");
-    $result = pg_query($conn, "SELECT * FROM ");
-    while ($row = pg_fetch_row($result)) {
-        echo "<p>" . htmlspecialchars($row[0]) . "</p>\n";
+    $host = "host=localhost";
+    $port = "port=5432";
+    $dbname = "dbname=postgres";
+    $user = "user=postgres";
+    $password = "password=1234";
+    $con = pg_connect("$host $port $dbname $user $password");
+    if($con){
+        echo "eso";
+    }else{
+        echo "nada";
     }
+
+
+    
+
+    
 ?>
